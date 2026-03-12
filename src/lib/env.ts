d0fa2ff044e7244f5bba580b721156d454a49de6
Abhimanyu Jangid <abhimanyujangid79@gmail.com>
2026-03-12 23:36:48 +0530

    feat: integrate tRPC with React Query for improved API handling and add necessary dependencies

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,10 +1,11 @@
-import { z } from 'zod';
-import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
 
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().min(1, "DATABASE_URL is required"),
+    APP_URL: z.string().min(1, "APP_URL is required"),
   },
-  experimental__runtimeEnv:{},
+  experimental__runtimeEnv: {},
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
